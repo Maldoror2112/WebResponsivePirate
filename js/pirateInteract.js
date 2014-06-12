529a9568dbb5e84764c012a1ae91b8302639d13f
Maldoror2112 <doug.hauger@gmail.com>
2014-06-12 14:55:22 -0700

    3dart added

--- js/pirateInteract.js
+++ js/pirateInteract.js
@@ -11,5 +11,21 @@ function initLinks(){
 		
 		return false;	
 	})
+	
+	/*function move_div(){
+		var windowWidth = $(window).width();
+		var windowHeight = $(window).height();
+		
+		objWidth = $('#article1-land').width();
+		objHeight = $('#article1-land').height();
+		
+		$('#article1-land').css('top' , (windowHeight/2) -(objHeight/2)).css('left' , (windowWidth/2) - (objWidth/2));		
+	}
+	
+	move_div();
+	
+	$(window).resize(function(){
+		move_div();
+	});*/
 }
 
